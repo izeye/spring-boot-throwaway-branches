f8955a5b0a4f2a9d084185c4f2286d0319d288f2
izeye <izeye@naver.com>
2015-09-19 21:46:55 +0900

    Create a project.

---
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -0,0 +1,16 @@
+package com.izeye.throwaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by izeye on 15. 9. 19..
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
