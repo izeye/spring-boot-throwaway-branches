8ce1668df6a8097b1b17158db119585ecf0689a9
izeye <izeye@naver.com>
2015-09-19 21:56:41 +0900

    Apply Lombok.

---
+++ src/main/java/com/izeye/throwaway/Person.java
@@ -0,0 +1,16 @@
+package com.izeye.throwaway;
+
+import lombok.Data;
+
+/**
+ * Created by izeye on 15. 9. 19..
+ */
+@Data
+public class Person {
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+}
---
+++ src/test/java/com/izeye/throwaway/PersonTests.java
@@ -0,0 +1,23 @@
+package com.izeye.throwaway;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by izeye on 15. 9. 19..
+ */
+public class PersonTests {
+	
+	@Test
+	public void test() {
+		long personId = 1L;
+		
+		Person person = new Person();
+		person.setId(personId);
+		
+		assertThat(person.getId(), is(personId));
+	}
+	
+}
