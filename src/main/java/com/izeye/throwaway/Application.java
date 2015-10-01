874ccba09189d6ef897bc430c43b6e3705404399
izeye <izeye@naver.com>
2015-10-01 16:02:40 +0900

    Fix collision between Spring Data JPA and Spring Data Elasticsearch.
    Relates to http://stackoverflow.com/questions/32878813/how-do-you-use-both-spring-data-jpa-and-spring-data-elasticsearch-repositories-o

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -2,11 +2,23 @@ package com.izeye.throwaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by izeye on 15. 9. 19..
  */
 @SpringBootApplication
+@EnableJpaRepositories(
+		includeFilters = @ComponentScan.Filter(
+				type = FilterType.ASSIGNABLE_TYPE, classes = JpaRepository.class))
+@EnableElasticsearchRepositories(
+		includeFilters = @ComponentScan.Filter(
+				type = FilterType.ASSIGNABLE_TYPE, classes = ElasticsearchRepository.class))
 public class Application {
 
 	public static void main(String[] args) {
