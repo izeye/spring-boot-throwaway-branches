c6e9e231c746a54036de5eb0ad9b0ffb24def6e9
izeye <izeye@naver.com>
2015-10-05 17:45:12 +0900

    Apply Spring Security (WIP)

---
+++ src/main/java/com/izeye/throwaway/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.izeye.throwaway;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by izeye on 15. 10. 3..
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
