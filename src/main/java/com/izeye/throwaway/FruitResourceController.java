25c30c6c1007bd2cdeab8f547188af22eb431369
Johnny Lim <izeye@naver.com>
2015-12-14 11:57:33 +0900

    Add a `Resources` sample

--- src/main/java/com/izeye/throwaway/FruitResourceController.java
+++ src/main/java/com/izeye/throwaway/FruitResourceController.java
@@ -1,10 +1,14 @@
 package com.izeye.throwaway;
 
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -16,17 +20,31 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RequestMapping(path = "/fruits")
 public class FruitResourceController {
 
+	@RequestMapping(method = RequestMethod.GET)
+	public Fruit[] fruits() {
+		return Fruit.values();
+	}
+
 	// NOTE: The `produces` attribute is only for browsers.
-	@RequestMapping(method = RequestMethod.GET, produces = MediaTypes.HAL_JSON_VALUE)
-	public Resource<Fruit[]> fruits() {
+	@RequestMapping(path = "/with-resource", method = RequestMethod.GET,
+			produces = MediaTypes.HAL_JSON_VALUE)
+	public Resource<Fruit[]> fruitsWithResource() {
 		Resource<Fruit[]> resource = new Resource<Fruit[]>(Fruit.values());
-		resource.add(linkTo(methodOn(FruitResourceController.class).fruits()).withSelfRel());
+		Link selfLink = linkTo(methodOn(FruitResourceController.class).fruitsWithResource())
+				.withSelfRel();
+		resource.add(selfLink);
 		return resource;
 	}
 
-	@RequestMapping(path = "/without-resource", method = RequestMethod.GET)
-	public Fruit[] fruitsWithoutResource() {
-		return Fruit.values();
+	// NOTE: The `produces` attribute is only for browsers.
+	@RequestMapping(path = "/with-resources", method = RequestMethod.GET,
+			produces = MediaTypes.HAL_JSON_VALUE)
+	public Resources<Fruit> fruitsWithResources() {
+		Resources<Fruit> resources = new Resources<Fruit>(Arrays.asList(Fruit.values()));
+		Link selfLink = linkTo(methodOn(FruitResourceController.class).fruitsWithResources())
+				.withSelfRel();
+		resources.add(selfLink);
+		return resources;
 	}
 	
 }
