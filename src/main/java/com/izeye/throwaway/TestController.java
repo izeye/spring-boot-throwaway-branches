f1eb45602a176f93e852651c4adb659a68594d7f
Johnny Lim <izeye@naver.com>
2016-01-07 16:40:42 +0900

    Add a custom RedirectView sample

--- src/main/java/com/izeye/throwaway/TestController.java
+++ src/main/java/com/izeye/throwaway/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Created by izeye on 15. 10. 7..
@@ -22,6 +23,24 @@ public class TestController {
 	@ResponseBody
 	public String post(@RequestParam String name) {
 		return name;
+	}
+	
+	@RequestMapping(path = "/redirect_to_test", method = RequestMethod.GET)
+	public String redirectToTest() {
+		return "redirect:/test";
+	}
+
+	@RequestMapping(path = "/redirect_to_does_not_exist", method = RequestMethod.GET)
+	public String redirectToDoesNotExist() {
+		return "redirect:/{doesNotExist}";
+	}
+
+	@RequestMapping(path = "/redirect_to_does_not_exist_with_custom_redirect_view",
+			method = RequestMethod.GET)
+	public RedirectView redirectToDoesNotExistWithCustomRedirectView() {
+		RedirectView redirectView = new RedirectView("{doesNotExist}");
+		redirectView.setExpandUriTemplateVariables(false);
+		return redirectView;
 	}
 	
 }
