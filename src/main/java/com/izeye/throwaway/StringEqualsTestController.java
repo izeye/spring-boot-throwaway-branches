ab31d86520e82f5ce91b04235b6776eda8eead55
Johnny Lim <izeye@naver.com>
2016-01-25 10:14:51 +0900

    Add a String.equals() test in Thymeleaf

---
+++ src/main/java/com/izeye/throwaway/StringEqualsTestController.java
@@ -0,0 +1,21 @@
+package com.izeye.throwaway;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by izeye on 16. 1. 25..
+ */
+@Controller
+@RequestMapping(path = "/test/string/equals")
+public class StringEqualsTestController {
+	
+	@RequestMapping
+	public String testStringEquals(@RequestParam String name, Model model) {
+		model.addAttribute("name", name);
+		return "test_string_equals";
+	}
+	
+}
