5b100051db36817214ad87dfdcb812e1bd0a12dd
Johnny Lim <izeye@naver.com>
2016-03-04 23:07:28 +0900

    Guava ClassPath doesn't work after upgrading 1.4.0.M1

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -1,7 +1,11 @@
 package com.izeye.throwaway;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.ClassPath;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 /**
  * Created by izeye on 15. 9. 19..
@@ -10,6 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		try {
+			ClassPath classPath = ClassPath.from(Application.class.getClassLoader());
+			ImmutableSet<ClassPath.ClassInfo> classes = classPath.getTopLevelClasses("com.izeye.throwaway");
+			System.out.println("classes: " + classes);
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
 		SpringApplication.run(Application.class, args);
 	}
 	
