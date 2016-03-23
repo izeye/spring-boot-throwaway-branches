f47fba1fbc2cde26020fb2008d6e15c0cc4df08b
Johnny Lim <izeye@naver.com>
2016-03-23 17:04:00 +0900

    Create a sample reproducing NoClassDefFoundError on MBeanFactory

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -1,5 +1,6 @@
 package com.izeye.throwaway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by izeye on 15. 9. 19..
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
+//		new MBeanFactory();
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Ok!");
+		System.exit(0);
 	}
 	
 }
