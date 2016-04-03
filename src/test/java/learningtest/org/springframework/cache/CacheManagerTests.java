4f74803ef6c1ec08e9d9fbe5e69ce205f666c6af
Johnny Lim <izeye@naver.com>
2016-04-03 14:29:25 +0900

    Add a cache sample which puts and gets a value without @Cacheable

--- src/test/java/learningtest/org/springframework/cache/CacheManagerTests.java
+++ src/test/java/learningtest/org/springframework/cache/CacheManagerTests.java
@@ -1,6 +1,7 @@
 package learningtest.org.springframework.cache;
 
 import com.izeye.throwaway.Application;
+import com.izeye.throwaway.domain.Person;
 import com.izeye.throwaway.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,8 @@ import org.springframework.cache.CacheManager;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by izeye on 16. 4. 2..
@@ -26,7 +29,7 @@ public class CacheManagerTests {
 	PersonService personService;
 	
 	@Test
-	public void test() {
+	public void testCacheNames() {
 		System.out.println(this.cacheManager);
 		
 		// ConcurrentMapCacheManager creates a cache after accessing @Cacheable method.
@@ -34,12 +37,26 @@ public class CacheManagerTests {
 		// due to its configuration file (ehcache.xml).
 //		personService.getPersonWithInterfaceCacheable();
 		
-		Collection<String> cacheNames = cacheManager.getCacheNames();
+		Collection<String> cacheNames = this.cacheManager.getCacheNames();
 		System.out.println("cacheNames: " + cacheNames);
 		for (String cacheName : cacheNames) {
-			Cache cache = cacheManager.getCache(cacheName);
+			Cache cache = this.cacheManager.getCache(cacheName);
 			System.out.println(cache);
 		}
+	}
+	
+	@Test
+	public void testPutAndGet() {
+		long id = 1L;
+		
+		Person person = new Person();
+		person.setId(id);
+
+		Cache cache = this.cacheManager.getCache("manualPersonCache");
+		System.out.println(cache);
+		
+		cache.put(person.getId(), person);
+		assertThat(cache.get(id, Person.class)).isEqualTo(person);
 	}
 	
 }
