2142279f8ce2e93c6e7532cc3ef83155c1902429
Johnny Lim <izeye@naver.com>
2016-04-13 12:00:50 +0900

    Add a banner but why does it have no color?

--- src/test/java/com/izeye/throwaway/config/AppConfigTests.java
+++ src/test/java/com/izeye/throwaway/config/AppConfigTests.java
@@ -1,12 +1,11 @@
 package com.izeye.throwaway.config;
 
-import com.izeye.throwaway.Application;
 import com.izeye.throwaway.service.DefaultPersonServiceConsumer;
 import com.izeye.throwaway.service.PersonServiceConsumer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Created by izeye on 16. 4. 4..
  */
 @RunWith(SpringRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@SpringBootTest
 public class AppConfigTests {
 	
 	@Autowired
--- src/test/java/com/izeye/throwaway/person/service/PersonServiceTests.java
+++ src/test/java/com/izeye/throwaway/person/service/PersonServiceTests.java
@@ -1,12 +1,11 @@
 package com.izeye.throwaway.person.service;
 
-import com.izeye.throwaway.Application;
 import com.izeye.throwaway.service.DefaultPersonService;
 import com.izeye.throwaway.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Created by izeye on 16. 4. 1..
  */
 @RunWith(SpringRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@SpringBootTest
 public class PersonServiceTests {
 	
 	@Autowired
