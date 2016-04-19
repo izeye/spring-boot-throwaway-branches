ac37310179908682f045009860185f1b1205ace3
Johnny Lim <izeye@naver.com>
2016-04-19 16:39:37 +0900

    Upgrade to Spring Boot 1.4.0.M2

--- src/test/java/com/izeye/throwaway/PersonRepositoryTests.java
+++ src/test/java/com/izeye/throwaway/PersonRepositoryTests.java
@@ -3,8 +3,8 @@ package com.izeye.throwaway;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -15,8 +15,8 @@ import static org.hamcrest.core.Is.is;
 /**
  * Created by izeye on 15. 10. 1..
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class PersonRepositoryTests {
 	
 	@Autowired
