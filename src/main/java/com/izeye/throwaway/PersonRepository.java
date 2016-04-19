95d768099ad259cef912c855b7cc1bd59962ef0b
Johnny Lim <izeye@naver.com>
2016-04-19 15:57:32 +0900

    After upgrading Spring Boot 1.4.0.M2, Spring Data REST's projection doesn't work

--- src/main/java/com/izeye/throwaway/PersonRepository.java
+++ src/main/java/com/izeye/throwaway/PersonRepository.java
@@ -2,14 +2,13 @@ package com.izeye.throwaway;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 /**
  * Created by izeye on 15. 10. 1..
  */
-@RepositoryRestResource(excerptProjection = GroupInlinedPerson.class)
+//@RepositoryRestResource(excerptProjection = GroupInlinedPerson.class)
 public interface PersonRepository extends JpaRepository<Person, Long> {
 	
 	List<Person> findByFavoriteFruit(@Param("favorite_fruit") Fruit favoriteFruit);
