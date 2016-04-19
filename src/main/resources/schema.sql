4a73cd75e6ba744334318fa6739a8d5ad3d97670
Johnny Lim <izeye@naver.com>
2016-04-19 17:22:48 +0900

    Upgrade to Spring Boot 1.4.0.M2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE person (
+CREATE TABLE IF NOT EXISTS person (
   id INT,
   first_name VARCHAR(100),
   last_name VARCHAR(100),
   age INT
 );
 
-CREATE TABLE animal (
+CREATE TABLE IF NOT EXISTS animal (
   id INT,
   first_name VARCHAR(100),
   last_name VARCHAR(100),
--- src/test/java/com/izeye/throwaway/repository/PersonRepositoryTests.java
+++ src/test/java/com/izeye/throwaway/repository/PersonRepositoryTests.java
@@ -1,20 +1,19 @@
 package com.izeye.throwaway.repository;
 
-import com.izeye.throwaway.Application;
 import com.izeye.throwaway.domain.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 /**
  * Created by izeye on 16. 3. 6..
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class PersonRepositoryTests {
 	
 	@Autowired
