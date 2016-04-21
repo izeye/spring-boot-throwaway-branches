87dd41efb86fdac7e52213b7fb471190c976622b
Johnny Lim <izeye@naver.com>
2016-04-21 22:30:48 +0900

    Add a `LiveBeansView` test

---
+++ src/test/java/org/springframework/context/support/LiveBeansViewTests.java
@@ -0,0 +1,46 @@
+package org.springframework.context.support;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by izeye on 16. 4. 21..
+ */
+public class LiveBeansViewTests {
+
+	@Test
+	public void test() {
+		AnnotationConfigApplicationContext context
+				= new AnnotationConfigApplicationContext();
+		context.register(TestConfiguration.class);
+		context.getBeanFactory().registerSingleton("bean2", new MyBean());
+		context.refresh();
+
+		LiveBeansView view = new LiveBeansView();
+		view.setApplicationContext(context);
+
+		String json = view.getSnapshotAsJson();
+		assertThat(json).contains("bean1");
+		
+		// FIXME: Should work with `contains()` not `doesNotContain()`.
+		assertThat(json).doesNotContain("bean2");
+	}
+	
+	@Configuration
+	public static class TestConfiguration {
+		
+		@Bean
+		public MyBean bean1() {
+			return new MyBean();
+		}
+		
+	}
+
+	private static class MyBean {
+	}
+	
+}
