d2ff41eaa55ad233a319dec9baa8bcdf16526caa
Johnny Lim <izeye@naver.com>
2016-04-25 17:38:43 +0900

    Add a Thymeleaf script test

---
+++ src/main/java/com/izeye/throwaway/ScriptTestController.java
@@ -0,0 +1,23 @@
+package com.izeye.throwaway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by izeye on 16. 4. 25..
+ */
+@Controller
+public class ScriptTestController {
+	
+	@Value("${server.port}")
+	private int port;
+	
+	@GetMapping("/test/script")
+	public String testScript(Model model) {
+		model.addAttribute("url", "http://localhost:" + port + "/js/test.js");
+		return "test_script";
+	}
+	
+}
