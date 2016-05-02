dddafffbec2b0ae50f4062f6a186d0573d50c36e
Johnny Lim <izeye@naver.com>
2016-05-02 15:19:20 +0900

    Add an `HttpServletRequest` test

--- src/main/java/com/izeye/throwaway/person/web/TestController.java
+++ src/main/java/com/izeye/throwaway/person/web/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,16 +17,29 @@ import java.util.Map;
 public class TestController {
 	
 	@GetMapping("/httpServletRequest")
-	public Map<String, String> testHttpServletRequest(HttpServletRequest request) {
+	public Map<String, Object> testHttpServletRequest(HttpServletRequest request) {
 		StringBuffer requestURL = request.getRequestURL();
 		String requestURI = request.getRequestURI();
 		String queryString = request.getQueryString();
-		
-		Map<String, String> response = new HashMap<>();
+
+		Map<String, String> headers = extractHeaders(request);
+
+		Map<String, Object> response = new HashMap<>();
 		response.put("requestURL", requestURL.toString());
 		response.put("requestURI", requestURI);
 		response.put("queryString", queryString);
+		response.put("headers", headers);
 		return response;
 	}
-	
+
+	private Map<String, String> extractHeaders(HttpServletRequest request) {
+		Enumeration<String> headerNames = request.getHeaderNames();
+		Map<String, String> headers = new HashMap<>();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			headers.put(headerName, request.getHeader(headerName));
+		}
+		return headers;
+	}
+
 }
