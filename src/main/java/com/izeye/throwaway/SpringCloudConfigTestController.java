4a59799ca12e74fd74396ddb7cbeeb76b126c000
Johnny Lim <izeye@naver.com>
2016-06-22 22:02:12 +0900

    Apply Spring Cloud Config

---
+++ src/main/java/com/izeye/throwaway/SpringCloudConfigTestController.java
@@ -0,0 +1,23 @@
+package com.izeye.throwaway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by izeye on 16. 6. 22..
+ */
+@RestController
+@RequestMapping(path = "/test/spring-cloud-config")
+public class SpringCloudConfigTestController {
+	
+	@Value("${my.name}")
+	private String myName;
+	
+	@GetMapping("/my-name")
+	public String myName() {
+		return myName;
+	}
+	
+}
