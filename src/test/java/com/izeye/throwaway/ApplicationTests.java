c45b60bf775474a133f5d6702d6dd93155b236c0
Johnny Lim <izeye@naver.com>
2016-09-10 15:26:40 +0900

    Add a DataSource test

---
+++ src/test/java/com/izeye/throwaway/ApplicationTests.java
@@ -0,0 +1,34 @@
+package com.izeye.throwaway;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link Application}.
+ *
+ * @author Johnny Lim
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class ApplicationTests {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	public void testDataSource() {
+		assertThat(this.dataSource).isInstanceOf(org.apache.tomcat.jdbc.pool.DataSource.class);
+		org.apache.tomcat.jdbc.pool.DataSource tomcatJdbcPoolDataSource =
+				(org.apache.tomcat.jdbc.pool.DataSource) this.dataSource;
+		assertThat(tomcatJdbcPoolDataSource.isTestOnBorrow()).isTrue();
+	}
+
+}
