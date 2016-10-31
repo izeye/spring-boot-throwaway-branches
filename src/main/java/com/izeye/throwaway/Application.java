580db4c16dda00b3e58fa8c206befe9e0ec5c64b
Johnny Lim <izeye@naver.com>
2016-10-31 19:23:47 +0900

    Use a custom data source

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -1,13 +1,12 @@
 package com.izeye.throwaway;
 
-import com.izeye.throwaway.domain.Person;
 import com.izeye.throwaway.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by izeye on 15. 9. 19..
@@ -20,8 +19,9 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Person> persons = personRepository.findAll();
-		System.out.println(persons);
+//		List<Person> persons = personRepository.findAll();
+//		System.out.println(persons);
+		TimeUnit.SECONDS.sleep(50);
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/izeye/throwaway/config/PersistenceConfig.java
+++ src/main/java/com/izeye/throwaway/config/PersistenceConfig.java
@@ -5,11 +5,14 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 /**
@@ -18,11 +21,17 @@ import javax.sql.DataSource;
 @Configuration
 @EnableConfigurationProperties(MyBatisProperties.class)
 // FIXME: See https://github.com/mybatis/spring/issues/58
-@Lazy
+//@Lazy
 public class PersistenceConfig {
 
 	@Autowired
 	private MyBatisProperties myBatisProperties;
+
+	@Bean
+	@ConfigurationProperties(prefix = "my.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
 
 	@Bean
 	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
