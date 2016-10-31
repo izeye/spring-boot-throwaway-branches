eaa6f9a0279dfaf8dd8422f6d73b85783de19fca
Johnny Lim <izeye@naver.com>
2016-10-31 19:49:49 +0900

    Fix wrong configuration

--- src/main/java/com/izeye/throwaway/config/PersistenceConfig.java
+++ src/main/java/com/izeye/throwaway/config/PersistenceConfig.java
@@ -10,9 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 /**
