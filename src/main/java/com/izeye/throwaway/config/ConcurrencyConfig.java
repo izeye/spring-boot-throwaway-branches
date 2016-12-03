2aa9823b9262bf24c20cdcd2bb2faeed6b350ea0
Johnny Lim <izeye@naver.com>
2016-12-03 11:44:09 +0900

    Add a test for @Scheduled

---
+++ src/main/java/com/izeye/throwaway/config/ConcurrencyConfig.java
@@ -0,0 +1,14 @@
+package com.izeye.throwaway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Configuration for concurrency.
+ *
+ * @author Johnny Lim
+ */
+@Configuration
+@EnableScheduling
+public class ConcurrencyConfig {
+}
---
+++ src/main/java/com/izeye/throwaway/config/ScheduledInConfig.java
@@ -0,0 +1,38 @@
+package com.izeye.throwaway.config;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.izeye.throwaway.domain.Person;
+
+/**
+ * Configuration having {@link Scheduled}.
+ *
+ * @author Johnny Lim
+ */
+@Configuration
+public class ScheduledInConfig {
+
+	@Bean
+	public Person person() {
+		Person person = new Person();
+		person.setFirstName("Johnny");
+		person.setLastName("Lim");
+		return person;
+	}
+
+	// Just to test if registering a bean and autowiring it
+	// in the same configuration work.
+	@Autowired
+	private Person person;
+
+	@Scheduled(cron = "0 * * * * ?")
+	public void doScheduledJob() {
+		System.out.println(new Date() + ": " + this.person);
+	}
+
+}
