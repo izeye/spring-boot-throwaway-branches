e6fcc476b07709f8c1f5474a82d7cee42dd70e07
Johnny Lim <izeye@naver.com>
2017-02-18 12:38:16 +0900

    Try alternative for @ConfigurationProperties locations

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -1,7 +1,11 @@
 package com.izeye.throwaway;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.izeye.throwaway.repository.BucketTestRepository;
 
 /**
  * Main class.
@@ -9,10 +13,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Johnny Lim
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	static {
+		System.setProperty("spring.config.location", "classpath:/repository/bucket/bucket.yml");
+	}
+
+	@Autowired
+	private BucketTestRepository bucketTestRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.bucketTestRepository);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+
 }
