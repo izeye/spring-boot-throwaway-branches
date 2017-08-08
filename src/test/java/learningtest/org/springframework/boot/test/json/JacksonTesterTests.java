9ae41390f946b28300d8557098779df9a2d44b87
Johnny Lim <izeye@naver.com>
2017-08-08 16:25:03 +0900

    Add a test for "spring.jackson.deserialization.use-long-for-ints"

--- src/test/java/learningtest/org/springframework/boot/test/json/JacksonTesterTests.java
+++ src/test/java/learningtest/org/springframework/boot/test/json/JacksonTesterTests.java
@@ -1,6 +1,7 @@
 package learningtest.org.springframework.boot.test.json;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
@@ -21,14 +22,28 @@ public class JacksonTesterTests {
 	@Autowired
 	private JacksonTester<Person> json;
 
+	@Autowired
+	private JacksonTester<Map<String, Object>> jsonForMap;
+
 	@Test
 	public void testParseObject() throws IOException {
-		String json = "{\"firstName\": \"Johnny\", \"lastName\": \"Lim\"}";
+		String json = "{\"firstName\": \"Johnny\", \"lastName\": \"Lim\", \"age\": 20}";
 
 		Person person = this.json.parseObject(json);
 
 		assertThat(person.getFirstName()).isEqualTo("Johnny");
 		assertThat(person.getLastName()).isEqualTo("Lim");
+	}
+
+	@Test
+	public void testParseObjectWithUseLongForInts() throws IOException {
+		String json = "{\"firstName\": \"Johnny\", \"lastName\": \"Lim\", \"id\": 1}";
+
+		Map<String, Object> person = this.jsonForMap.parseObject(json);
+
+		assertThat(person.get("firstName")).isEqualTo("Johnny");
+		assertThat(person.get("lastName")).isEqualTo("Lim");
+		assertThat(person.get("id")).isEqualTo(1L);
 	}
 
 	@Data
