d11d10e37ac560879c9fcce35af281b170299761
Johnny Lim <izeye@naver.com>
2017-09-06 17:47:20 +0900

    Print HttpServletRequest.toString()

--- src/main/java/com/izeye/throwaway/HomeController.java
+++ src/main/java/com/izeye/throwaway/HomeController.java
@@ -1,5 +1,7 @@
 package com.izeye.throwaway;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,11 @@ public class HomeController {
 	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello, world!";
+	}
+
+	@GetMapping("/httpServletRequest")
+	public String httpServletRequest(HttpServletRequest request) {
+		return request.toString();
 	}
 
 }
