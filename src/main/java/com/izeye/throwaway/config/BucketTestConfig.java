a1290672dceea98706b1a258f8a17e2628ea01ee
Johnny Lim <izeye@naver.com>
2017-10-12 14:43:35 +0900

    Remove ConfigurationProperties in favour of Yaml
    See https://stackoverflow.com/questions/42310839/what-is-the-best-alternative-for-configurationproperties-locations

--- src/main/java/com/izeye/throwaway/config/BucketTestConfig.java
+++ src/main/java/com/izeye/throwaway/config/BucketTestConfig.java
@@ -1,10 +1,15 @@
 package com.izeye.throwaway.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import com.izeye.throwaway.repository.YamlBucketTestRepository;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor;
 
 /**
  * Configuration for {@link com.izeye.throwaway.domain.BucketTest}.
@@ -14,12 +19,27 @@ import com.izeye.throwaway.repository.YamlBucketTestRepository;
 @Configuration
 public class BucketTestConfig {
 
+	private static final String RESOURCE_PATH = "repository/bucket/bucket.yml";
+
+	private final Yaml yaml = new Yaml(new CustomClassLoaderConstructor(
+			YamlBucketTestRepository.class, YamlBucketTestRepository.class.getClassLoader()));
+
 	@Bean
-	// FIXME: 'locations' has been deprecated, so it should be replaced with what?
-//	@ConfigurationProperties(locations = "classpath:/repository/bucket/bucket.yml")
-	@ConfigurationProperties
 	public YamlBucketTestRepository bucketTestRepository() {
-		return new YamlBucketTestRepository();
+		return load(RESOURCE_PATH);
+	}
+
+	private YamlBucketTestRepository load(String resourcePath) {
+		return (YamlBucketTestRepository) this.yaml.load(asInputStream(resourcePath));
+	}
+
+	private InputStream asInputStream(String resourcePath) {
+		try {
+			return new ClassPathResource(resourcePath).getInputStream();
+		}
+		catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 
 }
