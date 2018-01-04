43854e4a957b82be79f222cc422d2e4ca725efa3
Johnny Lim <izeye@naver.com>
2018-01-04 23:18:29 +0900

    Add a RestTemplate bean

--- src/main/java/com/izeye/throwaway/HomeController.java
+++ src/main/java/com/izeye/throwaway/HomeController.java
@@ -2,9 +2,11 @@ package com.izeye.throwaway;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Home {@link RestController}.
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/")
 public class HomeController {
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@GetMapping("/hello-world")
 	public String helloWorld() {
@@ -28,6 +33,11 @@ public class HomeController {
 	@GetMapping("/throwException")
 	public String throwException() {
 		throw new RuntimeException("Expected.");
+	}
+
+	@GetMapping("/invokeRestTemplate")
+	public String invokeRestTemplate() {
+		return this.restTemplate.getForObject("https://www.google.com/", String.class);
 	}
 
 }
