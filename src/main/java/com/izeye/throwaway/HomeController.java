2f0972ad8b00d796448f423bb9770fc3c39e0fe3
Johnny Lim <izeye@naver.com>
2018-05-09 11:43:11 +0900

    Add a Thymeleaf sample with map

--- src/main/java/com/izeye/throwaway/HomeController.java
+++ src/main/java/com/izeye/throwaway/HomeController.java
@@ -1,6 +1,10 @@
 package com.izeye.throwaway;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,7 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@GetMapping
-	public String home() {
+	public String home(Model model) {
+		Map<String, Object> someMap = new HashMap<>();
+		someMap.put("firstName", "Johnny");
+		someMap.put("lastName", "Lim");
+		someMap.put("age", 20);
+		model.addAttribute("someMap", someMap);
 		return "index";
 	}
 
