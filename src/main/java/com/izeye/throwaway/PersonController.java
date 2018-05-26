d0f454b1459a88ca4a55d3525198486873f56e2d
Johnny Lim <izeye@naver.com>
2018-05-26 12:43:51 +0900

    Polish

--- src/main/java/com/izeye/throwaway/PersonController.java
+++ src/main/java/com/izeye/throwaway/PersonController.java
@@ -1,5 +1,9 @@
 package com.izeye.throwaway;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +20,15 @@ public class PersonController {
 
 	// curl -v -XPOST -u "user:secret" -H "Accept: application/json" -H "Content-type: application/json" -d '{"firstName": "Johnny", "lastName": "Lim"}' "http://localhost:8080/persons"
 	@PostMapping
-	public Person post(@RequestBody Person person) {
+	public Person post(@RequestBody Person person, HttpServletRequest request) {
+		System.out.println("Headers:");
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			System.out.println(headerName + ": " + request.getHeader(headerName));
+		}
+
+		System.out.println("Request body:");
 		System.out.println(person);
 		return person;
 	}
