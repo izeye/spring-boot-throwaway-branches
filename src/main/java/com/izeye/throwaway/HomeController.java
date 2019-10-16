de3c62ef111f53678310572a7d54b29e9dc0384c
Johnny Lim <izeye@naver.com>
2019-10-16 20:15:21 +0900

    Experiment Cache-Control header

--- src/main/java/com/izeye/throwaway/HomeController.java
+++ src/main/java/com/izeye/throwaway/HomeController.java
@@ -1,7 +1,11 @@
 package com.izeye.throwaway;
 
+import java.time.Duration;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,6 +38,33 @@ public class HomeController {
 	@GetMapping("/echo")
 	public String echo(@RequestParam String value) {
 		return value;
+	}
+
+	@GetMapping("/responseEntity")
+	public ResponseEntity<String> responseEntity() {
+		return ResponseEntity.ok()
+				.body("Hello, world!");
+	}
+
+	@GetMapping("/responseEntityCacheControlEmpty")
+	public ResponseEntity<String> responseEntityCacheControlEmpty() {
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.empty())
+				.body("Hello, world!");
+	}
+
+	@GetMapping("/responseEntityCacheControlNoStore")
+	public ResponseEntity<String> responseEntityCacheControlNoStore() {
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.noStore())
+				.body("Hello, world!");
+	}
+
+	@GetMapping("/responseEntityCacheControlMaxAge")
+	public ResponseEntity<String> responseEntityCacheControlMaxAge() {
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(Duration.ofMinutes(10)))
+				.body("Hello, world!");
 	}
 
 }
---
+++ src/test/java/com/izeye/throwaway/HomeControllerTests.java
@@ -0,0 +1,60 @@
+package com.izeye.throwaway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Tests for {@link HomeController}.
+ *
+ * @author Johnny Lim
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void responseEntityNoCacheControl() throws Exception {
+		this.mockMvc.perform(get("/responseEntity"))
+				.andExpect(status().isOk())
+				// Spring Security does seem to add a "Cache-Control" header.
+				.andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+	}
+
+	@Test
+	public void responseEntityCacheControlEmpty() throws Exception {
+		this.mockMvc.perform(get("/responseEntityCacheControlEmpty"))
+				.andExpect(status().isOk())
+				// Spring Security does seem to add a "Cache-Control" header.
+				.andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
+	}
+
+	@Test
+	public void responseEntityCacheControlNoStore() throws Exception {
+		this.mockMvc.perform(get("/responseEntityCacheControlNoStore"))
+				.andExpect(status().isOk())
+				.andExpect(header().string(HttpHeaders.CACHE_CONTROL, "no-store"));
+	}
+
+	@Test
+	public void responseEntityCacheControlMaxAge() throws Exception {
+		this.mockMvc.perform(get("/responseEntityCacheControlMaxAge"))
+				.andExpect(status().isOk())
+				.andExpect(header().string(HttpHeaders.CACHE_CONTROL, "max-age=600"));
+	}
+
+}
