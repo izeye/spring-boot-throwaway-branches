cd1ded1c7490eb7139baf4b99c38591f313021f4
Johnny Lim <izeye@naver.com>
2019-10-18 17:33:08 +0900

    Add tests for MockHttpServletRequest

---
+++ src/test/java/learningtest/org/springframework/mock/web/MockHttpServletRequestTests.java
@@ -0,0 +1,31 @@
+package learningtest.org.springframework.mock.web;
+
+import java.util.Collections;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link MockHttpServletRequest}.
+ *
+ * @author Johnny Lim
+ */
+public class MockHttpServletRequestTests {
+
+	@Test
+	public void addHeaderAndRemoveHeader() {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		String headerName = "Test";
+
+		request.addHeader(headerName, "1");
+		request.addHeader(headerName, "2");
+		assertThat(Collections.list(request.getHeaders(headerName))).containsExactly("1", "2");
+
+		request.removeHeader(headerName);
+		assertThat(request.getHeaders(headerName).hasMoreElements()).isFalse();
+	}
+
+}
