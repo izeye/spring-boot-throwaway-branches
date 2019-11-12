fdc3f2a3a9e0c0bc2bf351782d33e0e16cb7a2b5
Johnny Lim <izeye@naver.com>
2019-11-12 13:47:40 +0900

    Exercise Spring Boot Actuator security

--- src/main/java/com/izeye/throwaway/SecurityConfig.java
+++ src/main/java/com/izeye/throwaway/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.izeye.throwaway;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,9 +15,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().regexMatchers("/persons").authenticated()
-				.and().httpBasic()
-				.and().csrf().disable();
+		http.requestMatcher(EndpointRequest.toAnyEndpoint())
+				.authorizeRequests((requests) -> requests.anyRequest().authenticated())
+				.httpBasic()
+				.and()
+				.csrf().disable();
 	}
 
 }
