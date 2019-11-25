b7742a03460bbd9f98f85995e0926205d6eaec61
Johnny Lim <izeye@naver.com>
2019-11-25 18:13:16 +0900

    Use @Value to work around a bug on @ConfigurationProperties
    See https://github.com/spring-projects/spring-boot/issues/15835

--- src/main/java/com/izeye/throwaway/config/PersistenceConfig.java
+++ src/main/java/com/izeye/throwaway/config/PersistenceConfig.java
@@ -5,10 +5,12 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.Resource;
 
 import javax.sql.DataSource;
 
@@ -24,12 +26,16 @@ public class PersistenceConfig {
 	@Autowired
 	private MyBatisProperties myBatisProperties;
 
+	// FIXME: See https://github.com/spring-projects/spring-boot/issues/15835
+	@Value("${mybatis.mapper-locations}")
+	private Resource[] mapperLocations;
+
 	@Bean
 	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
-		sqlSessionFactoryBean.setConfigLocation(myBatisProperties.getConfigLocation());
-		sqlSessionFactoryBean.setMapperLocations(myBatisProperties.getMapperLocations());
+		sqlSessionFactoryBean.setConfigLocation(this.myBatisProperties.getConfigLocation());
+		sqlSessionFactoryBean.setMapperLocations(this.mapperLocations);
 		sqlSessionFactoryBean.setVfs(SpringBootVFS.class);
 		return sqlSessionFactoryBean;
 	}
