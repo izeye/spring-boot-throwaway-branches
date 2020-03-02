26f062a7059c52fbfab7903bd082b0ff4342bfca
Johnny Lim <izeye@naver.com>
2020-03-03 06:29:45 +0900

    Upgrade to Spring Boot 2.3.0.M2
    This commit also cleans up stuff unrelated to JDBC.

--- src/test/java/com/izeye/throwaway/ApplicationTests.java
+++ src/test/java/com/izeye/throwaway/ApplicationTests.java
@@ -1,15 +1,12 @@
 package com.izeye.throwaway;
 
-import java.sql.SQLException;
-
 import javax.sql.DataSource;
 
-import org.springframework.beans.DirectFieldAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import org.junit.Ignore;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -29,23 +26,7 @@ public class ApplicationTests {
 
 	@Test
 	public void testDataSource() {
-		assertThat(this.dataSource).isInstanceOf(org.apache.tomcat.jdbc.pool.DataSource.class);
-		org.apache.tomcat.jdbc.pool.DataSource tomcatJdbcPoolDataSource =
-				(org.apache.tomcat.jdbc.pool.DataSource) this.dataSource;
-		assertThat(tomcatJdbcPoolDataSource.isTestOnBorrow()).isFalse();
-	}
-
-	// This throws the following exception:
-	// java.sql.SQLException: Unable to load authentication plugin 'caching_sha2_password'.
-	@Ignore
-	@Test
-	public void testTomcatJdbcPoolIsNotCreated() throws SQLException {
-		DirectFieldAccessor dfa = new DirectFieldAccessor(this.dataSource);
-		assertThat(dfa.getPropertyValue("pool")).isNull();
-
-		// Create pool explicitly.
-		((org.apache.tomcat.jdbc.pool.DataSource) this.dataSource).createPool();
-		assertThat(dfa.getPropertyValue("pool")).isNotNull();
+		assertThat(this.dataSource).isInstanceOf(HikariDataSource.class);
 	}
 
 }
