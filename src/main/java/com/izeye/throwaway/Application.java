23234a8473ffd81496e18f52e885215d0f5883db
Johnny Lim <izeye@naver.com>
2020-05-12 14:39:48 +0900

    Test Spring Boot Logback extensions

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -1,7 +1,11 @@
 package com.izeye.throwaway;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Main class.
@@ -9,7 +13,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Johnny Lim
  */
 @SpringBootApplication
+@Slf4j
 public class Application {
+
+	@Bean
+	ApplicationRunner applicationRunner() {
+		return (args) -> {
+			for (int i = 0; i < 100_000; i++) {
+				log.info("Hello, world!");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
