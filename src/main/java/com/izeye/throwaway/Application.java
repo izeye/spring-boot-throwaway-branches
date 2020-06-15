ab2a97ee3829794c16771ec047ac5f432a5a911c
Johnny Lim <izeye@naver.com>
2020-06-15 15:11:32 +0900

    Set file.encoding to UTF-8 for Jib image

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -12,6 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		String fileEncoding = System.getProperty("file.encoding");
+		if (!fileEncoding.equals("UTF-8")) {
+			throw new IllegalStateException("'file.encoding' should be 'UTF-8' but was '" + fileEncoding + "'.");
+		}
 		SpringApplication.run(Application.class, args);
 	}
 	
