fd17c995c218d29086a70f9e8011d916f93943a2
Johnny Lim <izeye@naver.com>
2020-06-21 12:43:20 +0900

    Add a sleeping endpoint for testing

--- src/main/java/com/izeye/throwaway/web/TestRestController.java
+++ src/main/java/com/izeye/throwaway/web/TestRestController.java
@@ -1,8 +1,10 @@
 package com.izeye.throwaway.web;
 
+import java.util.concurrent.TimeUnit;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,6 +22,12 @@ public class TestRestController {
 	public String requestHeader(@RequestHeader(name = "X-Test", required = false) String test) {
 		log.info("test: {}", test);
 		return test;
+	}
+
+	@GetMapping("/sleep")
+	public String requestHeader(@RequestParam int seconds) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(seconds);
+		return "Slept for " + seconds + " seconds.";
 	}
 
 }
