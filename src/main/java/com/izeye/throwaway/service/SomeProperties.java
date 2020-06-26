a793827bad02fb6fc35a5662f0ce3f7578521d93
Johnny Lim <izeye@naver.com>
2020-06-26 13:04:13 +0900

    Apply validation to @ConfigurationProperties

--- src/main/java/com/izeye/throwaway/service/SomeProperties.java
+++ src/main/java/com/izeye/throwaway/service/SomeProperties.java
@@ -1,6 +1,8 @@
 package com.izeye.throwaway.service;
 
+import javax.validation.constraints.Pattern;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * {@link ConfigurationProperties} for {@link SomeService}.
@@ -8,11 +10,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @author Johnny Lim
  */
 @ConfigurationProperties("some")
+@Validated
 public class SomeProperties {
 
 	/**
-	 * Token.
+	 * Token ("username:password").
 	 */
+	@Pattern(regexp = "\\w+:\\w+")
 	private String token;
 
 	public String getToken() {
