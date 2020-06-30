4654f15bde7bc25e4a5963c21ef2885242d83a6f
Johnny Lim <izeye@naver.com>
2020-07-01 00:44:29 +0900

    Use default server port

--- src/main/java/com/izeye/throwaway/ScriptTestController.java
+++ src/main/java/com/izeye/throwaway/ScriptTestController.java
@@ -1,23 +1,21 @@
 package com.izeye.throwaway;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * Created by izeye on 16. 4. 25..
+ * {@link Controller} for testing scripts.
+ *
+ * @author Johnny Lim
  */
 @Controller
 public class ScriptTestController {
-	
-	@Value("${server.port}")
-	private int port;
-	
+
 	@GetMapping("/test/script")
 	public String testScript(Model model) {
-		model.addAttribute("url", "http://localhost:" + port + "/js/test.js");
+		model.addAttribute("url", "/js/test.js");
 		return "test_script";
 	}
-	
+
 }
