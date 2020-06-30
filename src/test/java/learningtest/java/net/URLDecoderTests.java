1f898f0f914a0bcac19d858e8de1168fdcaa8cf8
Johnny Lim <izeye@naver.com>
2020-07-01 00:37:06 +0900

    Migrate tests to ues JUnit Jupiter

--- src/test/java/learningtest/java/net/URLDecoderTests.java
+++ src/test/java/learningtest/java/net/URLDecoderTests.java
@@ -1,24 +1,22 @@
 package learningtest.java.net;
 
-import org.junit.Test;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by izeye on 15. 10. 7..
  */
-public class URLDecoderTests {
+class URLDecoderTests {
 	
 	@Test
-	public void testDecode() throws UnsupportedEncodingException {
+	void testDecode() throws UnsupportedEncodingException {
 		String expected = "type=KOREAN&name=가&address=나";
 		String decoded = URLDecoder.decode("type=KOREAN&name=%EA%B0%80&address=%EB%82%98", "UTF-8");
 		System.out.println(decoded);
-		assertThat(decoded, is(expected));
+		assertThat(decoded).isEqualTo(expected);
 	}
 	
 }
--- src/test/java/learningtest/org/springframework/test/util/ReflectionTestUtilsTests.java
+++ src/test/java/learningtest/org/springframework/test/util/ReflectionTestUtilsTests.java
@@ -1,26 +1,25 @@
 package learningtest.org.springframework.test.util;
 
-import org.junit.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by izeye on 15. 10. 2..
  */
-public class ReflectionTestUtilsTests {
+class ReflectionTestUtilsTests {
 	
 	@Test
-	public void testGetField() {
+	void testGetField() {
 		AtomicBoolean created = (AtomicBoolean) ReflectionTestUtils.getField(
 				ApplicationPidFileWriter.class, "created");
-		assertThat(created.get(), is(ApplicationPidFileWriter.isCreated()));
+		assertThat(created.get()).isEqualTo(ApplicationPidFileWriter.isCreated());
 		
 		created.set(true);
-		assertThat(created.get(), is(ApplicationPidFileWriter.isCreated()));
+		assertThat(created.get()).isEqualTo(ApplicationPidFileWriter.isCreated());
 	}
 	
 	static class ApplicationPidFileWriter {
