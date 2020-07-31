878bdfcca9bf9145f1054f1a8c8d1210da7bc767
izeye <izeye@naver.com>
2020-07-31 23:42:59 +0900

    Add a failing WebFlux endpoint

--- src/main/java/com/izeye/throwaway/HomeController.java
+++ src/main/java/com/izeye/throwaway/HomeController.java
@@ -3,6 +3,9 @@ package com.izeye.throwaway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 /**
  * Home {@link RestController}.
@@ -21,6 +24,14 @@ public class HomeController {
 	@GetMapping("/throwException")
 	public String throwException() {
 		throw new RuntimeException("Expected.");
+	}
+
+	@GetMapping("/boom")
+	public Mono<String> boom() {
+		return Flux.just("boom1", "boom2")
+				.single()
+				.checkpoint("afterSingle")
+				.subscribeOn(Schedulers.parallel());
 	}
 
 }
