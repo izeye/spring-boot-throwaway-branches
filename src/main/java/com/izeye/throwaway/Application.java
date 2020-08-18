cafe58834325d0afc267d8a66d2aa7f8623eaf84
Johnny Lim <izeye@naver.com>
2020-08-18 17:10:23 +0900

    Print file.encoding system property
    This is for testing JAVA_TOOL_OPTIONS environment variable. For example:
    
    JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8

--- src/main/java/com/izeye/throwaway/Application.java
+++ src/main/java/com/izeye/throwaway/Application.java
@@ -12,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		String fileEncoding = System.getProperty("file.encoding");
+		System.out.println("file.encoding: " + fileEncoding);
+
 		SpringApplication.run(Application.class, args);
 	}
 	
