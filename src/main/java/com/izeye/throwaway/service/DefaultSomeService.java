826782946ff8e54b1306afb748dc9fffdaece357
Johnny Lim <izeye@naver.com>
2020-08-24 16:45:19 +0900

    Polish

--- src/main/java/com/izeye/throwaway/service/DefaultSomeService.java
+++ src/main/java/com/izeye/throwaway/service/DefaultSomeService.java
@@ -12,6 +12,12 @@ import org.springframework.stereotype.Service;
 @EnableConfigurationProperties(SomeProperties.class)
 public class DefaultSomeService implements SomeService {
 
+	private final SomeProperties properties;
+
+	public DefaultSomeService(SomeProperties properties) {
+		this.properties = properties;
+	}
+
 	@Override
 	public void doService() {
 	}
