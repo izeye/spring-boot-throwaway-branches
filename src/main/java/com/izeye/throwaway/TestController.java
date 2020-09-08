e0dba0996ee00fb2460f698473ccb95b210f72c5
Johnny Lim <izeye@naver.com>
2020-09-08 22:13:07 +0900

    Add tests for WebClient

--- src/main/java/com/izeye/throwaway/TestController.java
+++ src/main/java/com/izeye/throwaway/TestController.java
@@ -2,11 +2,14 @@ package com.izeye.throwaway;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * {@link Controller} for testing.
@@ -15,7 +18,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 @RequestMapping(path = "/test")
+@Slf4j
 public class TestController {
+
+	private final AtomicBoolean first = new AtomicBoolean(true);
 
 	@GetMapping("/redirect/parameter")
 	public String redirectForTestingParameters() {
@@ -47,6 +53,17 @@ public class TestController {
 	public String path(@PathVariable String value) {
 		System.out.println("value: " + value);
 		return "value: " + value;
+	}
+
+	@GetMapping("/first_delay")
+	@ResponseBody
+	public String firstDelay() throws InterruptedException {
+		log.info("firstDelay() has been invoked.");
+
+		if (this.first.getAndSet(false)) {
+			TimeUnit.SECONDS.sleep(1);
+		}
+		return "Completed.";
 	}
 
 }
---
+++ src/test/java/com/izeye/throwaway/TestControllerTests.java
@@ -0,0 +1,52 @@
+package com.izeye.throwaway;
+
+import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutException;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import org.junit.jupiter.api.Test;
+import reactor.netty.http.client.HttpClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * Tests for {@link TestController}.
+ *
+ * @author Johnny Lim
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TestControllerTests {
+
+	private final WebClient webClient;
+	private final int port;
+
+	TestControllerTests(@Autowired WebClient.Builder webClientBuilder, @LocalServerPort int port) {
+		HttpClient httpClient = HttpClient.create()
+				.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 500)
+				.doOnConnected((connection) -> connection.addHandlerLast(
+						new ReadTimeoutHandler(500, TimeUnit.MILLISECONDS)));
+		this.webClient = webClientBuilder.clientConnector(new ReactorClientHttpConnector(httpClient)).build();
+		this.port = port;
+	}
+
+	@Test
+	void firstDelay() {
+		String uri = "http://localhost:" + this.port + "/test/first_delay";
+		assertThatThrownBy(() -> this.webClient.get().uri(uri).retrieve().bodyToMono(String.class).block())
+				.isInstanceOf(ReadTimeoutException.class);
+	}
+
+	@Test
+	void firstDelayWithRetry() {
+		String uri = "http://localhost:" + this.port + "/test/first_delay";
+		String response = this.webClient.get().uri(uri).retrieve().bodyToMono(String.class).retry().block();
+		assertThat(response).isEqualTo("Completed.");
+	}
+
+}
