4481f4f60f00060e368fd4e805592172f42d456b
izeye <izeye@naver.com>
2020-12-30 08:32:18 +0900

    Migrate tests to JUnit Jupiter

--- src/test/java/com/izeye/throwaway/learningtest/org/springframework/boot/test/autoconfigure/orm/jpa/DataJpaTestTests.java
+++ src/test/java/com/izeye/throwaway/learningtest/org/springframework/boot/test/autoconfigure/orm/jpa/DataJpaTestTests.java
@@ -1,9 +1,7 @@
 package com.izeye.throwaway.learningtest.org.springframework.boot.test.autoconfigure.orm.jpa;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -11,20 +9,21 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Created by izeye on 16. 6. 23..
+ * Tests for {@link DataJpaTest}.
+ *
+ * @author Johnny Lim
  */
-@RunWith(SpringRunner.class)
 @DataJpaTest
-public class DataJpaTestTests {
+class DataJpaTestTests {
 	
 	@Test
-	public void test() {
+	void test() {
 		assertThat(TransactionSynchronizationManager.isActualTransactionActive()).isTrue();
 	}
 	
 	@Test
 	@Transactional(propagation = Propagation.NOT_SUPPORTED)
-	public void testNonTransactional() {
+	void testNonTransactional() {
 		assertThat(TransactionSynchronizationManager.isActualTransactionActive()).isFalse();
 	}
 	
