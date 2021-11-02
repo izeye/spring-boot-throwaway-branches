3fca170fa328ade0cfb02bed92a7b88e7aaee165
izeye <izeye@naver.com>
2021-11-02 18:29:10 +0900

    Add a test for OutputCaptureExtension

---
+++ src/test/java/learningtest/org/springframework/boot/test/system/OutputCaptureExtensionTests.java
@@ -0,0 +1,25 @@
+package learningtest.org.springframework.boot.test.system;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link OutputCaptureExtension}.
+ *
+ * @author Johnny Lim
+ */
+class OutputCaptureExtensionTests {
+
+    @Test
+    @ExtendWith(OutputCaptureExtension.class)
+    void getOut(CapturedOutput output) {
+        String message = "Hello, world!";
+        System.out.println(message);
+        assertThat(output.getOut()).contains(message);
+    }
+
+}
